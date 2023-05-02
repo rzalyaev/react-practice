b9eecb4976afb84038f4a036d27970e283fb0553
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-03 02:57:28 +0400

    Add OnOff components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
+import {AppTitle} from "./components/AppTitle/AppTitle";
 import {Rating} from './components/Rating/Rating';
 import {Accordion} from "./components/Accordion/Accordion";
+import {OnOff} from "./components/OnOff/OnOff";
 
-function App() {
+const App = () => {
     return (
         <div className="App">
-            <Rating/>
-            <Accordion/>
+            <AppTitle/>
+            <Accordion title={'Menu'} collapsed={false}/>
+            <Accordion title={'Options'} collapsed={true}/>
+            <Rating value={3}/>
+            <OnOff isOn={true}/>
+            <OnOff isOn={false}/>
         </div>
     );
 }
