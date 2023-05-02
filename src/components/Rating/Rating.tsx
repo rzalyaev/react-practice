bea7458401417e4c948d8267e8241a949ed35510
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-03 02:57:47 +0400

    Add types (Lesson 4)

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import {Star} from "./Star/Star";
 
-export const Rating = () => {
+type PropsType = {
+    value: 0 | 1 | 2 | 3 | 4 | 5
+}
+
+export const Rating = (props: PropsType) => {
     return(
         <div>
             <Star/>
