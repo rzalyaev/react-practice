1abeeb4dc8bbd96ec629186a81f40cce0b766a84
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-07 16:39:49 +0400

    Hide Accordion and Rating. Add UncontrolledAccordion and UncontrolledRating

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import {AppTitle} from "./components/AppTitle/AppTitle";
 import {Rating} from './components/Rating/Rating';
 import {Accordion} from "./components/Accordion/Accordion";
 import {OnOff} from "./components/OnOff/OnOff";
+import {UncontrolledAccordion} from "./components/UncontrolledAccordion/UncontrolledAccordion";
+import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
 
 const App = () => {
     return (
         <div className="App">
             <AppTitle/>
-            <Accordion title={'Menu'} collapsed={false}/>
-            <Accordion title={'Options'} collapsed={true}/>
-            <Rating value={3}/>
+            {/*<Accordion title={'Menu'} collapsed={false}/>*/}
+            {/*<Accordion title={'Options'} collapsed={true}/>*/}
+            <UncontrolledAccordion title={'Menu'}/>
+            <UncontrolledAccordion title={'Options'}/>
+            {/*<Rating value={3}/>*/}
+            <UncontrolledRating/>
             <OnOff isOn={true}/>
             <OnOff isOn={false}/>
         </div>
