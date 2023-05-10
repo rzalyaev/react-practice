c47a5d6e77668662d6447e48b16c2befbdba110d
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-10 06:44:10 +0400

    Add onClick event

--- src/components/Accordion/AccordionTitle/AccordionTitle.tsx
+++ src/components/Accordion/AccordionTitle/AccordionTitle.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import styles from "./AccordionTitle.module.css";
 
 type PropsType = {
     title: string
     collapsed: boolean
+    setCollapsed: () => void
 }
 
 export const AccordionTitle = (props: PropsType) => {
+    const changeCollapsedState = () => props.setCollapsed()
+
     return(
         <div>
-            <h3>{props.title}</h3>
-            <p>{props.collapsed && <i> collapsed</i>}</p>
+            <h3 className={styles.accordionTitle} onClick={changeCollapsedState}>
+                {props.title} {props.collapsed ? "+" : "-"}
+            </h3>
         </div>
     )
 }
