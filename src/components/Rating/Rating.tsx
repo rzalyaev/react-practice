7c3c294ec4054311716cb179d2d6129f82e6a2a5
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-10 06:47:05 +0400

    Add props for correct component work

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import {Star} from "./Star/Star";
 
+export type RatingType = 0 | 1 | 2 | 3 | 4 | 5
+
 type PropsType = {
-    value: 0 | 1 | 2 | 3 | 4 | 5
+    rating: RatingType
+    setRating: (newRatingValue: RatingType) => void
 }
 
 export const Rating = (props: PropsType) => {
     return(
         <div>
-            <Star selected={props.value > 0}/>
-            <Star selected={props.value > 1}/>
-            <Star selected={props.value > 2}/>
-            <Star selected={props.value > 3}/>
-            <Star selected={props.value > 4}/>
+            <h3>Controlled Rating</h3>
+            <Star selected={props.rating > 0} setRating={() => props.setRating(1)}/>
+            <Star selected={props.rating > 1} setRating={() => props.setRating(2)}/>
+            <Star selected={props.rating > 2} setRating={() => props.setRating(3)}/>
+            <Star selected={props.rating > 3} setRating={() => props.setRating(4)}/>
+            <Star selected={props.rating > 4} setRating={() => props.setRating(5)}/>
         </div>
     )
 }
