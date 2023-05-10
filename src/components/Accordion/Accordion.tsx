5acccb82ec70493f7cac5dca2cb7e9c895aee1d3
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-10 06:43:27 +0400

    Add props for correct component work

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -5,12 +5,15 @@ import {AccordionBody} from "./AccordionBody/AccordionBody";
 type PropsType = {
     title: string
     collapsed: boolean
+    setCollapsed: () => void
 }
 
 export const Accordion = (props: PropsType) => {
     return (
         <div>
-            <AccordionTitle title={props.title} collapsed={props.collapsed}/>
+            <AccordionTitle title={props.title}
+                            collapsed={props.collapsed}
+                            setCollapsed={props.setCollapsed}/>
             {!props.collapsed && <AccordionBody/>}
         </div>
     )
