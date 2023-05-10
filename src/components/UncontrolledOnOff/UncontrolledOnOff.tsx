6f8991de217fc344061a7b72bd7f8cc04a5ef60b
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-10 06:43:40 +0400

    Initial commit

---
+++ src/components/UncontrolledOnOff/UncontrolledOnOff.tsx
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+import styles from "../OnOff/OnOff.module.css";
+
+export const UncontrolledOnOff = () => {
+    const [switchState, setSwitchState] = useState<boolean>(false);
+
+    const turnOn = () => setSwitchState(true)
+    const turnOff = () => setSwitchState(false)
+    const onButtonClassName = `${styles.on} ${!switchState && styles.inactive}`
+    const offButtonClassName = `${styles.off} ${switchState && styles.inactive}`
+    const indicatorClassName = `${styles.indicator} ${switchState ? styles.indicatorOn : styles.indicatorOff}`
+    return(
+        <div>
+            <h3>Uncontrolled OnOff</h3>
+            <div className={styles.onOff}>
+                <div className={onButtonClassName} onClick={turnOn}>
+                    On
+                </div>
+                <div className={offButtonClassName} onClick={turnOff}>
+                    Off
+                </div>
+                <div className={indicatorClassName}></div>
+            </div>
+        </div>
+    )
+}
