d536241d315c7fc68c5c98521b0a901dffdaf52f
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-10 06:47:18 +0400

    Add onClick event

--- src/components/Rating/Star/Star.tsx
+++ src/components/Rating/Star/Star.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import styles from "./Star.module.css";
 
 type PropsType = {
     selected: boolean
+    setRating: () => void
 }
 
 export const Star = (props: PropsType) => {
+    const onClickHandler = () => props.setRating()
+    const selectedStarClassName = `${styles.star} ${props.selected && styles.selectedStar}`
+
     return(
-        <span>{props.selected ? <b>star</b> : <>star</>} </span>
+        <span className={selectedStarClassName} onClick={onClickHandler}>
+            {props.selected ? <b>star</b> : "star"}
+        </span>
     )
 }
