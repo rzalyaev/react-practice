dfee8f4bd2832ccdd249923e83bfd6b89d93b8e3
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-10 06:48:04 +0400

    Refactor code

--- src/components/UncontrolledAccordion/UncontrolledAccordionTitle/UncontrolledAccordionTitle.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordionTitle/UncontrolledAccordionTitle.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import styles from "../../Accordion/AccordionTitle/AccordionTitle.module.css";
 
 type PropsType = {
     title: string
     collapsed: boolean
-    onClickCallback: () => void
+    setCollapsed: () => void
 }
 
 export const UncontrolledAccordionTitle = (props: PropsType) => {
+    const changeCollapsedState = () => props.setCollapsed()
+
     return(
         <div>
-            <h3 onClick={props.onClickCallback}>{props.title}</h3>
-            <p>{props.collapsed && <i> collapsed</i>}</p>
+            <h3 className={styles.accordionTitle} onClick={changeCollapsedState}>
+                {props.title} {props.collapsed ? "+" : "-"}
+            </h3>
         </div>
     )
 }
