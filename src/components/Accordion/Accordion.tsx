b4111454461444bbb838aed7462c68020efd284c
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-20 16:44:16 +0400

    Add props for interacting with data

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import {AccordionTitle} from "./AccordionTitle/AccordionTitle";
 import {AccordionBody} from "./AccordionBody/AccordionBody";
 
+export type ItemType = {
+    title: string,
+    value: any,
+}
+
 type PropsType = {
     title: string
     collapsed: boolean
     setCollapsed: () => void
+    items: ItemType[],
+    onClick: (value: any) => void,
 }
 
 export const Accordion = (props: PropsType) => {
@@ -14,7 +21,7 @@ export const Accordion = (props: PropsType) => {
             <AccordionTitle title={props.title}
                             collapsed={props.collapsed}
                             setCollapsed={props.setCollapsed}/>
-            {!props.collapsed && <AccordionBody/>}
+            {!props.collapsed && <AccordionBody items={props.items} onClick={props.onClick}/>}
         </div>
     )
 }
