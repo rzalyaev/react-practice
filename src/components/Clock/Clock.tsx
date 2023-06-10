f96097415249fa185408dd8cab811f794dc3bc32
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-10 22:36:08 +0400

    Create Clock component

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from "react";
+
+export const Clock = () => {
+    const [date, setDate] = useState<Date>(new Date());
+
+    useEffect(() => {
+        const intervalId = window.setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+        <time>{date.toLocaleTimeString("ru")}</time>
+    );
+}
