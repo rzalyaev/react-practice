bdc1f63ecc0d9db0eec05edab50b2e43da22f595
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-14 22:16:32 +0400

    Add two types of the Clock component instead of base example

--- src/components/Clock/Clock.stories.tsx
+++ src/components/Clock/Clock.stories.tsx
@@ -6,6 +6,10 @@ export default {
     component: Clock
 }
 
-export const BaseExample = () => {
-    return <Clock/>
+export const DigitalType = () => {
+    return <Clock clockType={'digital'}/>
+}
+
+export const AnalogType = () => {
+    return <Clock clockType={'analog'}/>
 }
