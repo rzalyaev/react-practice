3c265681d87be73c19ebd2b5ef130d60972e9b58
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-18 23:08:33 +0400

    Add CountrySelect story

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -1,4 +1,5 @@
 import React, {useCallback, useMemo, useState} from "react";
+import {Select} from "../components/Select/Select";
 
 export default {
     title: 'useMemo demo'
@@ -111,3 +112,47 @@ const BooksSecret = (props: BooksSecretPropsType) => {
 }
 
 const Books = React.memo(BooksSecret);
+
+export const CountrySelect = () => {
+    const cities = [
+        {countryId: 7, value: '1', title: 'Moscow', population: 11980},
+        {countryId: 7, value: '2', title: 'Izhevsk', population: 620},
+        {countryId: 7, value: '3', title: 'Chelyabinsk', population: 1182},
+        {countryId: 380, value: '4', title: 'Minsk', population: 1975},
+        {countryId: 380, value: '5', title: 'Gomel', population: 508},
+        {countryId: 380, value: '6', title: 'Vitebsk', population: 362},
+        {countryId: 375, value: '7', title: 'Kiev', population: 2884},
+        {countryId: 375, value: '8', title: 'Odessa', population: 993},
+        {countryId: 375, value: '9', title: 'Dnepr', population: 966},
+    ]
+
+    const citiesFromBelarus = useMemo(() => cities.filter(city => city.countryId === 380), []);
+    const citiesStartsWithM = useMemo(() => cities.filter(city => city.title.startsWith('M')), []);
+    const citiesWithPopulationMoreThan1Mil = useMemo(() => cities.filter(city => city.population > 1000), []);
+
+    const [count, setCount] = useState(0);
+    const [value1, setValue1] = useState<string>(citiesFromBelarus[0].value);
+    const [value2, setValue2] = useState<string>(citiesStartsWithM[0].value);
+    const [value3, setValue3] = useState<string>(citiesWithPopulationMoreThan1Mil[0].value);
+
+    return (
+        <div>
+            <div>
+                Счётчик: {count}
+                <button onClick={() => setCount(count + 1)}>+ 1</button>
+            </div>
+            <div>
+                Города из Беларуси:
+                <Select value={value1} onChange={setValue1} items={citiesFromBelarus}/>
+            </div>
+            <div>
+                Города на букву М:
+                <Select value={value2} onChange={setValue2} items={citiesStartsWithM}/>
+            </div>
+            <div>
+                Города с численностью более 1 миллиона человек:
+                <Select value={value3} onChange={setValue3} items={citiesWithPopulationMoreThan1Mil}/>
+            </div>
+        </div>
+    )
+}
