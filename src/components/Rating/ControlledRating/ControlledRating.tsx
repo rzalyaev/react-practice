ac6551d1ead01c094ac7f32e2f1669f4bf118f83
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-27 22:30:50 +0400

    Add header

--- src/components/Rating/ControlledRating/ControlledRating.tsx
+++ src/components/Rating/ControlledRating/ControlledRating.tsx
@@ -7,13 +7,16 @@ type ControlledRatingPropsType = {
 export const ControlledRating = (props: ControlledRatingPropsType) => {
   console.log('ControlledRating is rendering');
   return (
-      <div>
-        <Star selected={props.value >= 1}/>
-        <Star selected={props.value >= 2}/>
-        <Star selected={props.value >= 3}/>
-        <Star selected={props.value >= 4}/>
-        <Star selected={props.value === 5}/>
-      </div>
+      <>
+        <h3>Controlled rating (rating fixed at {props.value})</h3>
+        <div>
+          <Star selected={props.value >= 1}/>
+          <Star selected={props.value >= 2}/>
+          <Star selected={props.value >= 3}/>
+          <Star selected={props.value >= 4}/>
+          <Star selected={props.value === 5}/>
+        </div>
+      </>
   )
 }
 
