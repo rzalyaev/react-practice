1d05d5243e03ec92a6b37dba8b3f593aad3d4cf9
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-27 19:57:35 +0400

    Remove App content. Add OnOff component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Accordion} from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
+import {OnOff} from "./components/OnOff/OnOff";
 
 function App() {
   console.log('App is rendering');
   return (
       <div className="App">
-        <PageTitle title={'This is APP title!'}/>
-        <PageTitle title={'Random title!'}/>
-        <Accordion titleValue={'Menu'} collapsed={false}/>
-        <Accordion titleValue={'Users'} collapsed={true}/>
-        <Rating value={0}/>
-        <Rating value={1}/>
-        <Rating value={2}/>
-        <Rating value={3}/>
-        <Rating value={4}/>
-        <Rating value={5}/>
+        <OnOff/>
       </div>
   );
 }
