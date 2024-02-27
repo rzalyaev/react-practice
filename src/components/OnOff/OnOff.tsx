d9f578c62da68d5216e4557e2ca44fa8e7c3c35d
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-27 19:53:45 +0400

    Create OnOff component

---
+++ src/components/OnOff/OnOff.tsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import styles from './OnOff.module.css';
+
+export const OnOff = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  const turnOn = () => setIsOn(true);
+  const turnOff = () => setIsOn(false);
+
+  const onButtonClass = `${styles.onButton} ${isOn ? styles.isOn: ''}`;
+  const offButtonClass = `${styles.offButton} ${isOn ? '': styles.isOff}`;
+  const indicatorClass = `${styles.indicator} ${isOn ? styles.isOn : styles.isOff}`;
+
+  return (
+      <div className={styles.wrapper}>
+        <button className={onButtonClass} onClick={turnOn}>On</button>
+        <button className={offButtonClass} onClick={turnOff}>Off</button>
+        <div className={indicatorClass}></div>
+      </div>
+  );
+};
