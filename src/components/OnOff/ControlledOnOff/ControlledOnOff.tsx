560a9e644086fa4a93b8340ce8b62585e24b42ff
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-29 17:40:16 +0400

    Create ControlledOnOff component

--- src/components/OnOff/ControlledOnOff/ControlledOnOff.tsx
+++ src/components/OnOff/ControlledOnOff/ControlledOnOff.tsx
@@ -1,24 +1,26 @@
-import React, {useState} from 'react';
-import styles from './OnOff.module.css';
+import React from 'react';
+import styles from './ControlledOnOff.module.css';
 
-export const OnOff = () => {
-  const [isOn, setIsOn] = useState(false);
+type ControlledOnOffPropsType = {
+  isOn: boolean
+  turnOn: () => void
+  turnOff: () => void
+}
 
-  const turnOn = () => setIsOn(true);
-  const turnOff = () => setIsOn(false);
-
+export const ControlledOnOff = ({isOn, turnOn, turnOff}: ControlledOnOffPropsType) => {
   const onButtonClass = `${styles.onButton} ${isOn ? styles.isOn: ''}`;
   const offButtonClass = `${styles.offButton} ${isOn ? '': styles.isOff}`;
   const indicatorClass = `${styles.indicator} ${isOn ? styles.isOn : styles.isOff}`;
 
   return (
-      <>
-        <h3>On-off switcher</h3>
+      <div>
+        <h3>Controlled On-off switcher</h3>
+        <h4>State is managed from the <u>outside</u> of a component</h4>
         <div className={styles.wrapper}>
           <button className={onButtonClass} onClick={turnOn}>On</button>
           <button className={offButtonClass} onClick={turnOff}>Off</button>
           <div className={indicatorClass}></div>
         </div>
-      </>
+      </div>
   );
 };
