a3d56c59cf1448be8407e0491c15a5c1960c6fc0
rzalyaev <ruslan.zalyaev@gmail.com>
2024-02-29 17:44:39 +0400

    Add destructuring assignment and changeCollapseState function

--- src/components/Accordion/ControlledAccordion/ControlledAccordion.tsx
+++ src/components/Accordion/ControlledAccordion/ControlledAccordion.tsx
@@ -3,36 +3,35 @@ import React from "react";
 type ControlledAccordionPropsType = {
   titleValue: string
   collapsed: boolean
+  changeCollapsedState: () => void
 }
 
-export const ControlledAccordion = (props: ControlledAccordionPropsType) => {
-  console.log('ControlledAccordion is rendering');
+export const ControlledAccordion = ({titleValue, collapsed, changeCollapsedState}: ControlledAccordionPropsType) => {
   return (
       <div>
-        <ControlledAccordionTitle title={props.titleValue}/>
-        {!props.collapsed && <ControlledAccordionBody/>}
+        <ControlledAccordionTitle title={titleValue} changeCollapsedState={changeCollapsedState}/>
+        {!collapsed && <ControlledAccordionBody/>}
       </div>
   )
 }
 
 type ControlledAccordionTitlePropsType = {
   title: string
+  changeCollapsedState: () => void
 }
 
-const ControlledAccordionTitle = (props: ControlledAccordionTitlePropsType) => {
-  console.log('ControlledAccordionTitle is rendering');
+const ControlledAccordionTitle = ({title, changeCollapsedState}: ControlledAccordionTitlePropsType) => {
   return (
-      <h3>{props.title}</h3>
+      <h3 style={{cursor: 'pointer'}} onClick={changeCollapsedState}>{title}</h3>
   )
 }
 
 const ControlledAccordionBody = () => {
-  console.log('ControlledAccordionBody is rendering');
   return (
       <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
       </ul>
   )
 }
