f2017e763d0c8ac144c9f9a1bc9bf59b6ab07469
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-07 23:56:08 +0400

    Add items to props

--- src/components/Accordion/ControlledAccordion/ControlledAccordion.stories.tsx
+++ src/components/Accordion/ControlledAccordion/ControlledAccordion.stories.tsx
@@ -1,7 +1,7 @@
 import type {Meta, StoryObj} from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import { ControlledAccordion } from './ControlledAccordion';
+import {ControlledAccordion, ItemType} from './ControlledAccordion';
 import {useState} from "react";
 
 const meta: Meta<typeof ControlledAccordion> = {
@@ -18,18 +18,44 @@ export const FirstStory: Story = {
   }
 }
 
+const onClickCallback = action('some item was clicked');
+
 export const Collapsed = () => {
-  return <ControlledAccordion titleValue={'Collapsed Accordion'} collapsed={true} onChange={action('onClick')}/>
+  return <ControlledAccordion titleValue={'Collapsed Accordion'}
+                              collapsed={true}
+                              onChange={action('onClick')}
+                              items={[]}
+                              onClick={onClickCallback}
+  />
 }
 
 export const Expanded = () => {
-  return <ControlledAccordion titleValue={'Expanded Accordion'} collapsed={false} onChange={() => {}}/>
+  const items: ItemType[] = [
+    {title: 'Dima', value: 1},
+    {title: 'Valera', value: 2},
+    {title: 'Artem', value: 3},
+    {title: 'Viktor', value: 4}
+  ]
+  return <ControlledAccordion titleValue={'Expanded Accordion'}
+                              collapsed={false}
+                              onChange={() => {}}
+                              items={items}
+                              onClick={onClickCallback}
+  />
 }
 
 export const Clickable = () => {
+  const items: ItemType[] = [
+    {title: 'Dima', value: 1},
+    {title: 'Valera', value: 2},
+    {title: 'Artem', value: 3},
+    {title: 'Viktor', value: 4}
+  ]
   const [collapsed, setCollapsed] = useState<boolean>(false);
   return <ControlledAccordion titleValue={'Accordion (click to collapse/expand)'}
                               collapsed={collapsed}
                               onChange={() => setCollapsed(!collapsed)}
+                              items={items}
+                              onClick={onClickCallback}
   />
 }
