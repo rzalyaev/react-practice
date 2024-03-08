b161f6b1da57fb2996fd6246a20881c38517dd05
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-08 23:46:31 +0400

    Change filtration

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,10 +1,12 @@
 import React, {useState, KeyboardEvent} from "react";
 import styles from './Select.module.css';
+import collapseArrow from '../_assets/icons8-collapse-arrow-15.png';
+import expandArrow from '../_assets/icons8-expand-arrow-15.png';
 import {ItemType} from "../Accordion/ControlledAccordion/ControlledAccordion";
 
 type SelectPropsType = {
-  value: any
-  onChange: (value: any) => void
+  value?: string
+  onChange: (value: string) => void
   items: ItemType[]
 }
 
@@ -14,25 +16,26 @@ export const Select = ({value, onChange, items}: SelectPropsType) => {
   const handleOpenCloseSelectByClick = () => setCollapsed(!collapsed);
   const handleOpenCloseSelectByEnterPress = (event: KeyboardEvent<HTMLDivElement>) => {
     if (event.key === 'Enter') {
-      setCollapsed(!collapsed);
+      handleOpenCloseSelectByClick();
     }
   }
   const mappedItems = items
-      .filter(i => i.value !== value)
+      .filter((i, index) => value ? (i.value !== value) : (index !== 0))
       .map(i => {
-    const chooseItem = (newValue: any) => {
+    const chooseItem = (newValue: string) => {
       onChange(newValue);
       setCollapsed(!collapsed);
     }
     const handleOnItemClick = () => chooseItem(i.value);
-    return <div onClick={handleOnItemClick}>{i.title}</div>
+    return <div onClick={handleOnItemClick} className={styles.itemToSelect}>{i.title}</div>
   });
+  const selectedClassName: string = `${styles.selected} ${!collapsed && styles.expanded}`;
   return (
-      <div>
-        <div onClick={handleOpenCloseSelectByClick}
-             onKeyUp={handleOpenCloseSelectByEnterPress}
-             className={styles.selected}
-        >{selectedItemTitle?.title || 'none'}</div>
+      <div className={styles.wrapper} tabIndex={1} onKeyUp={handleOpenCloseSelectByEnterPress}>
+        <div onClick={handleOpenCloseSelectByClick} className={selectedClassName}>
+          {selectedItemTitle?.title || items[0].title}
+          {collapsed ? <img src={expandArrow} alt='expand arrow'/> : <img src={collapseArrow} alt="collapse arrow"/>}
+        </div>
         {!collapsed && mappedItems}
       </div>
   )
