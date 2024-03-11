ad129fd3d2396af9d83aea87dec5268c9e253eb1
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-11 22:53:49 +0400

    Add mouse enter and key up handlers

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,6 +1,5 @@
 import React, {useState, KeyboardEvent} from "react";
 import styles from './Select.module.css';
-import collapseArrow from '../_assets/icons8-collapse-arrow-15.png';
 import expandArrow from '../_assets/icons8-expand-arrow-15.png';
 import {ItemType} from "../Accordion/ControlledAccordion/ControlledAccordion";
 
@@ -12,29 +11,64 @@ type SelectPropsType = {
 
 export const Select = ({value, onChange, items}: SelectPropsType) => {
   const [collapsed, setCollapsed] = useState(true);
+  const [hoveredItemValue, setHoveredItemValue] =
+      useState<string>(value || items[0].value);
   const selectedItemTitle = items.find(item => item.value === value);
-  const handleOpenCloseSelectByClick = () => setCollapsed(!collapsed);
-  const handleOpenCloseSelectByEnterPress = (event: KeyboardEvent<HTMLDivElement>) => {
-    if (event.key === 'Enter') {
-      handleOpenCloseSelectByClick();
+  const handleOnClick = () => {
+    setCollapsed(!collapsed);
+    if (hoveredItemValue !== value) {
+      if (value !== undefined) {
+        setHoveredItemValue(value);
+      } else {
+        setHoveredItemValue(items[0].value);
+      }
     }
   }
-  const mappedItems = items
-      .filter((i, index) => value ? (i.value !== value) : (index !== 0))
-      .map(i => {
+  const handleOnKeyUp = (event: KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'Enter') {
+      setCollapsed(!collapsed);
+      onChange(hoveredItemValue);
+    }
+    if (event.key === 'ArrowDown') {
+      if (hoveredItemValue === items[items.length - 1].value) {
+        setHoveredItemValue(items[0].value);
+      } else {
+        setHoveredItemValue(String(Number(hoveredItemValue) + 1));
+      }
+    }
+    if (event.key === 'ArrowUp') {
+      if (hoveredItemValue === items[0].value) {
+        setHoveredItemValue(items[items.length - 1].value);
+      } else {
+        setHoveredItemValue(String(Number(hoveredItemValue) - 1));
+      }
+    }
+  }
+
+  const mappedItems = items.map(i => {
     const chooseItem = (newValue: string) => {
       onChange(newValue);
       setCollapsed(!collapsed);
     }
-    const handleOnItemClick = () => chooseItem(i.value);
-    return <div onClick={handleOnItemClick} className={styles.itemToSelect}>{i.title}</div>
+    const handleOnClick = () => chooseItem(i.value);
+    const handleOnMouseEnter = () => setHoveredItemValue(i.value);
+
+    const itemClassName: string = hoveredItemValue === i.value
+        ? `${styles.itemToSelect} ${styles.selectedItem}`
+        : styles.itemToSelect;
+
+    return (
+        <div onClick={handleOnClick} onMouseEnter={handleOnMouseEnter} className={itemClassName}>{i.title}</div>
+    )
   });
+
   const selectedClassName: string = `${styles.selected} ${!collapsed && styles.expanded}`;
+
   return (
-      <div className={styles.wrapper} tabIndex={1} onKeyUp={handleOpenCloseSelectByEnterPress}>
-        <div onClick={handleOpenCloseSelectByClick} className={selectedClassName}>
+      <div className={styles.wrapper} tabIndex={0} onKeyUp={handleOnKeyUp}>
+        <div onClick={handleOnClick} className={selectedClassName}>
           {selectedItemTitle?.title || items[0].title}
-          {collapsed ? <img src={expandArrow} alt='expand arrow'/> : <img src={collapseArrow} alt="collapse arrow"/>}
+          <img src={expandArrow} alt='expand arrow'/>
         </div>
         {!collapsed && mappedItems}
       </div>
