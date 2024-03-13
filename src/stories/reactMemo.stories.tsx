a37667eb3e458b9c27fbe4e6390aa8ea4e2aea3f
rzalyaev <ruslan.zalyaev@gmail.com>
2024-03-13 23:53:26 +0400

    Create react memo stories

---
+++ src/stories/reactMemo.stories.tsx
@@ -0,0 +1,40 @@
+import React, {useState, memo} from "react";
+import type {Meta} from "@storybook/react";
+import {Input} from "../components/Input/Input";
+
+const meta: Meta<typeof Input> = {
+  component: Input,
+};
+
+export default meta;
+
+const NewMessagesCounter = (props: any) => {
+  console.log('NewMessageCounter component rendered');
+  return <div>{props.count}</div>
+}
+const MemoizedNewMessagesCounter = memo(NewMessagesCounter);
+
+const Users = (props: {users: string[]}) => {
+  console.log('Users component rendered');
+  return (
+      <div>
+        {props.users.map((u, i) => <div key={i}>{u}</div>)}
+      </div>
+  )
+}
+const MemoizedUsers = memo(Users);
+
+export const Example1 = () => {
+  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem']);
+  const handlePlusOne = () => setCount(prevState => prevState + 1);
+  const handleAddAndrew = () => setUsers([...users, 'Andrew']);
+  return (
+      <>
+        <button onClick={handlePlusOne}>+</button>
+        <MemoizedNewMessagesCounter count={count}/>
+        <button onClick={handleAddAndrew}>Add Andrew</button>
+        <MemoizedUsers users={users}/>
+      </>
+  )
+}
